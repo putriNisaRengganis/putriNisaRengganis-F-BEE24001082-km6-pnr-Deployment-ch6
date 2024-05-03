cb97fa8645c3bbf5ca4110767fe6eb9b9bf98e94
“putriNisaRengganis” <putrinisarengganis@gmail.com>
2024-05-03 22:52:36 +0700

    fix : server start

--- app.js
+++ app.js
@@ -17,6 +17,4 @@ app.put('/images/:id', updateImages);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => {
-    console.log('Server is running on port 3000');
-});
+module.exports = app;
